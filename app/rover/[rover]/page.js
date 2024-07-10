c941ffa47eef39242580e50237b4109fe3840240
Wir3s <113479857+Wir3s@users.noreply.github.com>
2024-07-10 07:00:48 -0500

    Adding camera selection options

--- app/rover/[rover]/page.js
+++ app/rover/[rover]/page.js
@@ -1,12 +1,26 @@
-import { Suspense } from "react";
+"use client";
+import { useState, useEffect } from "react";
 import { getRoverData } from "../../../actions/getRoverData";
 import PhotoCard from "../../components/PhotoCard";
 import Link from "next/link";
 import styles from "./Rover.module.css";
 
-export default async function Rover({ params }) {
+export default function Rover({ params }) {
   const { rover } = params;
-  const photos = await getRoverData(rover);
+  const [photos, setPhotos] = useState([]);
+  const [camera, setCamera] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getRoverData(rover, camera);
+      setPhotos(data);
+    }
+    fetchData();
+  }, [rover, camera]);
+
+  const handleCameraChange = (event) => {
+    setCamera(event.target.value);
+  };
 
   return (
     <div>
@@ -17,6 +31,20 @@ export default async function Rover({ params }) {
         <h1 className={styles.headerTitle}>
           {rover.charAt(0).toUpperCase() + rover.slice(1)} Latest Photos
         </h1>
+        <select value={camera} onChange={handleCameraChange}>
+          <option value="">All Cameras</option>
+          <option value="fhaz">Front Hazard Avoidance Camera</option>
+          <option value="rhaz">Rear Hazard Avoidance Camera</option>
+          <option value="mast">Mast Camera</option>
+          <option value="chemcam">Chemistry and Camera Complex</option>
+          <option value="mahli">Mars Hand Lens Imager</option>
+          <option value="mardi">Mars Descent Imager</option>
+          <option value="navcam">Navigation Camera</option>
+          <option value="pancam">Panoramic Camera</option>
+          <option value="minites">
+            Miniature Thermal Emission Spectrometer
+          </option>
+        </select>
       </div>
       <Suspense fallback={<p>Loading...</p>}>
         <PhotoGallery photos={photos} />
